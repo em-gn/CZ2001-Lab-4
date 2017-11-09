99e0bb2940c359886b77b50391e1da9714ce6d62
EOWD0001 <EOWD0001@e.ntu.edu.sg>
2017-11-09 15:51:54 +0800

    Implemented printGraph()

--- src/Graph.java
+++ src/Graph.java
@@ -10,6 +10,10 @@ public class Graph
 {
     private int noOfVertices;
     private ArrayList<Integer>[] vertexList;
+    
+    // Hard-coded names of cities
+	List<String> cityList = Arrays.asList("Singapore","Kuala Lumpur", "Jakarta","Bangkok","Hanoi","Manila","Beijing","Pyongyang","Seoul","Taipei","Hong Kong","Tokyo","Sydney","Perth","New Zealand","Washington D.C.","New York","Los Angeles","Chicago","Texas","Seattle","Boston","London","Amsterdam","Berlin","Copenhagen","Moscow","Luxembourg","Paris","Rome","Stockholm","Vatican City","Toronto","Shanghai","Cairo","Istanbul","Dubai","Macau","Las Vegas","Prague","Barcelona","Venice","Budapest","Munich","Zurich","Chiang Mai","Vancouver","Melbourne","Rio De Janeiro","Frankfurt");
+	
  
     public Graph(int noOfCities)
     {
@@ -20,43 +24,49 @@ public class Graph
         }
     }
 
-    void addEdge(int firstVertex,int secondVertex)
+    public void addEdge(int firstVertex,int secondVertex)
     {
     	vertexList[firstVertex].add(secondVertex);
     }
- 
+    
+    public void printGraph () {
+    	System.out.println("\nNow printing Graph:\n");
+    	for (int i = 0; i < vertexList.length; i++) {
+    		System.out.format("[%2d]: %15s -->", i, cityList.get(i)); // Prints the city name of the vertex
+    		for (Integer cityIndex : vertexList[i]) {
+        		System.out.format(" [%2s - %s]", cityIndex, cityList.get(cityIndex));
+    		}
+    		System.out.println();
+    	}
+    }
     /*
      * BFS Algorithm
      * */
-    void BFS(int sourceVertex)
+    public void BFS(int sourceVertex)
     {
         /*
          * By default, all vertices start off as not visited (i.e. false)
          * */
-        boolean visited[] = new boolean[noOfVertices];
+        boolean visitedVertices[] = new boolean[noOfVertices];
  
         // Queue for BFS
         LinkedList<Integer> queue = new LinkedList<Integer>();
  
         // Visit sourceVertex and mark it in visited as true
-        visited[sourceVertex]=true;
+        visitedVertices[sourceVertex]=true;
         queue.add(sourceVertex);
  
         while (queue.size() != 0)
         {
-        	/* Dequeue a vertex from the queue to check
-        	 * */
         	sourceVertex = queue.poll();
-            System.out.print(sourceVertex + " ");
- 
             /* For all adjacent vertices (adjVertex) of the dequeued sourceVertex:
              * If an adjVertex has not been visited, then mark it as visited and queue it
              */ 
             Iterator<Integer> iter = vertexList[sourceVertex].listIterator();
             while (iter.hasNext()) {
             	Integer adjVertex = iter.next();
-            	if (!visited[adjVertex]){
-            		visited[adjVertex] = true;
+            	if (!visitedVertices[adjVertex]){
+            		visitedVertices[adjVertex] = true;
             		queue.add(adjVertex);
                 }
             }
@@ -67,16 +77,14 @@ public class Graph
     public static void main(String args[])
     {
     	int c;
-    	List<String> cityList = Arrays.asList("Singapore","Kuala Lumpur", "Jakarta","Bangkok","Hanoi","Manila","Beijing","Pyongyang","Seoul","Taipei","Hong Kong","Tokyo","Sydney","Perth","New Zealand","Washington D.C.","New York","Los Angeles","Chicago","Texas","Seattle","Boston","London","Amsterdam","Berlin","Copenhagen","Moscow","Luxembourg","Paris","Rome","Stockholm","Vatican City","Toronto","Shanghai","Cairo","Istanbul","Dubai","Macau","Las Vegas","Prague","Barcelona","Venice","Budapest","Munich","Zurich","Chiang Mai","Vancouver","Melbourne","Rio De Janeiro","Frankfurt"); //tbc, hardcoded
-    	
+
     	Scanner sc = new Scanner(System.in);
     	Random rand = new Random();
     	System.out.println("How many cities?");
     	
-    	//creat graph of size c
+    	// Create Graph of size c
     	c = sc.nextInt();
         Graph g = new Graph(c);
-        
         
         //init arraylist of pairs
         List<Pair> combinationList = new ArrayList<Pair>();
@@ -117,11 +125,12 @@ public class Graph
         	g.addEdge(param1, param2);
         }
         
-
- 
+        g.printGraph();
+        
+        /*
         System.out.println("Following is Breadth First Traversal "+
                            "(starting from vertex 2)");
- 
         g.BFS(2);
+        */
     }
 }
