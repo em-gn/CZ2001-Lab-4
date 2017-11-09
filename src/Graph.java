2abc6fc7dbc646b01fe588f703d19810afcba329
EOWD0001 <EOWD0001@e.ntu.edu.sg>
2017-11-09 18:24:47 +0800

    Re-implementation of BFS and a whole lot of stuff

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,7 +17,6 @@ public class Graph
     // Hard-coded names of cities
 	List<String> cityList = Arrays.asList("Singapore","Kuala Lumpur", "Jakarta","Bangkok","Hanoi","Manila","Beijing","Pyongyang","Seoul","Taipei","Hong Kong","Tokyo","Sydney","Perth","New Zealand","Washington D.C.","New York","Los Angeles","Chicago","Texas","Seattle","Boston","London","Amsterdam","Berlin","Copenhagen","Moscow","Luxembourg","Paris","Rome","Stockholm","Vatican City","Toronto","Shanghai","Cairo","Istanbul","Dubai","Macau","Las Vegas","Prague","Barcelona","Venice","Budapest","Munich","Zurich","Chiang Mai","Vancouver","Melbourne","Rio De Janeiro","Frankfurt");
 	
- 
     public Graph(int noOfCities)
     {
     	this.noOfVertices = noOfCities;
@@ -34,103 +36,196 @@ public class Graph
     	for (int i = 0; i < vertexList.length; i++) {
     		System.out.format("[%2d]: %15s -->", i, cityList.get(i)); // Prints the city name of the vertex
     		for (Integer cityIndex : vertexList[i]) {
-        		System.out.format(" [%2s - %s]", cityIndex, cityList.get(cityIndex));
+        		System.out.format(" [%2s - %s]", cityIndex, cityList.get(cityIndex)); // Prints adjacent cities
     		}
     		System.out.println();
     	}
     }
-    /*
-     * BFS Algorithm
-     * */
-    public void BFS(int sourceVertex)
+    
+    public void printGraph (boolean printAsCondensed) {
+    	if (printAsCondensed == true) {
+        	System.out.println("\nNow printing Graph (Condensed Form):\n");
+        	for (int i = 0; i < vertexList.length; i++) {
+        		System.out.format("[%2d]: %15s -->", i, cityList.get(i)); // Prints the city name of the vertex
+        		for (Integer cityIndex : vertexList[i]) {
+            		System.out.format(" [%2s]", cityIndex, cityList.get(cityIndex)); // Prints adjacent cities
+        		}
+        		System.out.println();
+        	}
+    	}
+    	else {
+    		printGraph();
+    	}
+    }
+    
+    public void printCities () {
+    	System.out.println("\nNow printing list of cities:\n");
+    	for (int i = 0; i < cityList.size(); i++) {
+    		System.out.format("[%2d]: %s\n", i, cityList.get(i));
+    	}
+    	System.out.println();
+    }
+    
+    public void printCities (int sizeOfGraph) {
+    	System.out.println("\nNow printing list of cities (reduced):\n");
+    	for (int i = 0; i < sizeOfGraph; i++) {
+    		System.out.format("[%2d]: %s\n", i, cityList.get(i));
+    	}
+    	System.out.println();
+    }
+    
+    public int[][] generateMatrixOfEdges () {	
+		int [][] matrixToBeGenerated = new int[noOfVertices][noOfVertices];
+		Random rand = new Random();
+		
+		int a, b;
+		
+		/* This generates a random noOfVertices x noOfVertices matrix filled with 0 or 1
+		 * 0 = edge is absent (there is no flight connecting the two cities)
+		 * 1 = edge is present (there is a flight connecting the two cities)
+		 */
+		for(a = 0; a < noOfVertices; a++ ) {
+			for(b = 0; b < noOfVertices; b++ ) {
+				matrixToBeGenerated[a][b] = rand.nextInt(2);
+			}
+		}
+		
+		/*
+		 * The graph we are simulating is an undirected graph, hence we take one half of the matrix and reflect that
+		 * half across the diagonal to get a matrix that is symmetrical with respect to the main diagonal. In this
+		 * graph, it also does not make sense to have a self-edge (i.e. an edge from one node back to itself) and hence
+		 * all [a][a] vertices in the matrix are by default set to 0.
+		 * 
+		 * */
+		for(a = 0; a < noOfVertices; a++) {
+			for (b = 0; b < a; b++) {
+				matrixToBeGenerated[b][a] = matrixToBeGenerated[a][b]; // Reflect matrix w.r.t. main diagonal
+			}
+			matrixToBeGenerated[a][a] = 0; // No self-edges
+		}
+		
+		// For debugging purposes - prints a visual representation of the matrix
+		System.out.println("\nNow printing Matrix:\n");
+		for(a = 0; a < noOfVertices; a++ ) {
+			for(b = 0; b < noOfVertices; b++ ) {
+				System.out.format("[%d]", matrixToBeGenerated[a][b]);
+			}
+			System.out.println();
+		}
+		
+		return matrixToBeGenerated;
+    }
+    
+    public void BFS(int sourceVertex, int destinationVertex)
     {
-        /*
-         * By default, all vertices start off as not visited (i.e. false)
-         * */
+    	// Flag to be set when a shortest path to destination is found
+    	boolean shortestPathFound = false;
+    	
+    	// By default, all vertices start off as not visited (i.e. false)
         boolean visitedVertices[] = new boolean[noOfVertices];
  
-        // Queue for BFS
+        // Queue for BFS Algorithm
         LinkedList<Integer> queue = new LinkedList<Integer>();
- 
-        // Visit sourceVertex and mark it in visited as true
+        
+        // Maps every new adjVertex to the one that was visited before it
+        // Using this, we can trace back a path (shortest path) to the source
+        // In this map: key is adjVertex (after) while currentVertex (before) is the value (see below)
+        Map<Integer, Integer> previousVertices = new HashMap<Integer, Integer>();
+        
+        // Visit sourceVertex, mark it in visitedVertices as true, then add it to queue
         visitedVertices[sourceVertex]=true;
         queue.add(sourceVertex);
- 
+        
+        // currentVertex is used to iterate through the while loop below
+        int currentVertex = sourceVertex;
+        
         while (queue.size() != 0)
         {
-        	sourceVertex = queue.poll();
-            /* For all adjacent vertices (adjVertex) of the dequeued sourceVertex:
-             * If an adjVertex has not been visited, then mark it as visited and queue it
-             */ 
-            Iterator<Integer> iter = vertexList[sourceVertex].listIterator();
+        	currentVertex = queue.poll();
+        	
+        	if (currentVertex == destinationVertex) {
+        		shortestPathFound = true;
+        		break;
+        	}
+        	
+            // For all adjacent vertices (adjVertex) of the dequeued currentVertex:
+            // If an adjVertex has not been visited, then mark it as visited and queue it
+            Iterator<Integer> iter = vertexList[currentVertex].listIterator();
             while (iter.hasNext()) {
             	Integer adjVertex = iter.next();
-            	if (!visitedVertices[adjVertex]){
+            	if (visitedVertices[adjVertex] == false){
             		visitedVertices[adjVertex] = true;
             		queue.add(adjVertex);
+            		previousVertices.put(adjVertex, currentVertex);
+            		if (adjVertex == destinationVertex) {
+            			shortestPathFound = true;
+            			break;
+            		}
                 }
             }
         }
+        
+        if (shortestPathFound) {
+        	ArrayList<Integer> pathToTrace = new ArrayList<Integer>();
+        	Integer cityToTrace = currentVertex;
+        	while (cityToTrace != null) {
+        		pathToTrace.add(cityToTrace);
+        		cityToTrace = previousVertices.get(cityToTrace);
+        	}
+        	Collections.reverse(pathToTrace);
+        	System.out.println("\nNow printing trace of shortest path:\n");
+        	for (Integer cityIndex : pathToTrace) {
+        		System.out.format("%2d : %s\n", cityIndex, cityList.get(cityIndex));
+        	}
+        }
+        else {
+        	System.out.println("\nPath not found\n");
+        }
     }
 
- // Driver method to
+
     public static void main(String args[])
     {
-    	int c;
-
     	Scanner sc = new Scanner(System.in);
-    	Random rand = new Random();
+    	
+    	int sizeOfGraph;
+    	int source, destination;
+    	
     	System.out.println("How many cities?");
     	
     	// Create Graph of size c
-    	c = sc.nextInt();
-        Graph g = new Graph(c);
+    	sizeOfGraph = sc.nextInt();
+    	sc.nextLine(); // Consume \n
+        Graph g = new Graph(sizeOfGraph);
         
-        //init arraylist of pairs
-        List<Pair> combinationList = new ArrayList<Pair>();
         
-        //create list of all combinations for 0-c
-        int i = 0;
-        int j = i + 1;
-        while(true) {
-        	combinationList.add(new Pair(i,j));
-        	j++;
-        	if(j >= c) {
-        		i++;
-        		j = i + 1;
+        /* Edges are represented in matrix form (i.e. matrix[a][b] where a and b are cities)
+         * This is where we insert each of these edges into the Graph
+         * */
+        int[][] matrixOfEdges = g.generateMatrixOfEdges();
+        int a, b;
+        for (a = 0; a < sizeOfGraph; a++) {
+        	for (b = 0; b < sizeOfGraph; b++) {
+        		if (matrixOfEdges[a][b] == 1) {
+            		g.addEdge(a, b);
         		}
-        	if(i >= c - 1) {
-        		break;
-        		}
-        	}
-        
-        //combinationID list to check if that combination has been used before (no duplicates)
-        int[] combinationID = new int[combinationList.size()];
-    	for (int index = 0;index<combinationList.size();index++) {
-    		combinationID[index]=1;
-    	}
-        
-        //inserting edges into graph
-        for (int index = 0;index<combinationID.length*2; index++) {
-        	int param1 = -1;
-        	int param2 = -1;
-        	while (param1 == -1) {
-        		if (combinationID[rand.nextInt(combinationID.length)] == 1) {
-        			Pair temp = combinationList.get(i);
-        			param1 = temp.x;
-        			param2 = temp.y;
-        			combinationID[i] = -1;
+        		else {
+        			continue;
         		}
         	}
-        	g.addEdge(param1, param2);
         }
         
-        g.printGraph();
+        // For debug purposes. Used to display the graph visually.
+        g.printGraph(true);
         
-        /*
-        System.out.println("Following is Breadth First Traversal "+
-                           "(starting from vertex 2)");
-        g.BFS(2);
-        */
+        g.printCities(sizeOfGraph);
+        System.out.println("\nWhich city (vertex) to select as source? (please input the number associated with the city)");
+        source = sc.nextInt();
+        sc.nextLine(); // Consume \n
+        System.out.println("\nWhich city (vertex) to select as destination? (please input the number associated with the city)");
+        destination = sc.nextInt();
+        sc.nextLine(); // Consume \n
+        
+        g.BFS(source, destination);
     }
 }
